4ae02765e9305c638e1dabc292baf4a32a295c79
siddhikharde <siddhikharde2005@gmail.com>
2025-11-17 22:37:31 +0530

    routing using react library done

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,20 @@
 import { createRoot } from 'react-dom/client'
-import Navbar from './navbar'
+import {BrowserRouter, Routes, Route} from 'react-router'
 import Home from './home'
 import About from './about' 
 import Contact from './contact'
-import Footer from './footer'
 
-const root=createRoot(document.getElementById('root'));
-const path=window.location.pathname;
 
-if(path=="/"){
-  root.render(<Home/>);
-}else if(path=="/about"){
-  root.render(<About/>);
-}
-else if(path=="/contact"){
-  root.render(<Contact/>);
-}
-else{
-  root.render(
-    <div>
-      <h1>Error 404:
-        Page Not Found
-      </h1>
-    </div>
-  )
-}
+createRoot(document.getElementById('root')).render(
+ 
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} /> 
+    </Routes>
+  </BrowserRouter>
+)
+
+
+
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './navbar.css'
+import { Link } from "react-router";
 function Navbar({activMenu}) {
   return (
     <div>
         <div className="navbar">
             <div className="logo">CodeVerse</div>
             <div className="links">
-                <a href="/" className={`menu ${activMenu==="/" ? "menu-active" : ""}`}>Home</a>
-                <a href="/about" className={`menu ${activMenu==="/about" ? "menu-active":""}`}>About</a>
-                <a href="/contact" className={`menu ${activMenu==="/contact" ?"menu-active":""}`}>Contact</a>
+                <Link to="/" className={`menu ${activMenu==="/" ? "menu-active" : ""}`}>Home</Link>
+                <Link to="/about" className={`menu ${activMenu==="/about" ? "menu-active":""}`}>About</Link>
+                <Link to="/contact" className={`menu ${activMenu==="/contact" ?"menu-active":""}`}>Contact</Link>
             </div>
         </div>
       
